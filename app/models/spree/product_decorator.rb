dc8c8ff104af2c01f0cc7506ab8b7ea4ed7dd255
Ajmal-hassan <ajmalhassan651@gmail.com>
2020-09-08 13:57:07 +0500

    update slide with spree 4.0

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,8 +1,12 @@
-Spree::Product.class_eval do
-  has_one :slide, dependent: :destroy
-  after_update :destroy_slide_if_deleted
+module Spree::ProductDecorator
+  def self.prepended(base)
+    base.has_one :slide, dependent: :destroy
+    base.after_update :destroy_slide_if_deleted
+  end
 
   def destroy_slide_if_deleted
     slide.update_attributes(published: false) if slide && deleted_at
   end
 end
+
+Spree::Product.prepend Spree::ProductDecorator
